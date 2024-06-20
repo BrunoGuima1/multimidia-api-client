217e17fbc652a88b5a825209820fe926cad219b1
Bruno Henrique Guimarães <bruno.guima212@gmail.com>
2024-06-19 21:05:08 -0300

    vue funcionando

---
+++ src/views/HomeView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+  <h1 class="head">Personagens Dragon Ball</h1>
+
+  <h4 class="type">Cartões</h4>
+
+  <div class="container">
+    <div v-for="person in people" :key="person.id">
+      <card :person="person"></card>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+import Card from '../components/Card.vue'
+
+const API_URL = `https://dragonball-api.com/api/characters`
+
+export default {
+  data: () => ({
+    name: '',
+    people: []
+
+
+  }),
+  methods: {
+    async getPeople () {
+      const response = await fetch(API_URL)
+      const data = await response.json()
+
+      this.people = data.items
+    }
+  },
+  created() {
+    this.getPeople()
+  },
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: black;
+}
+
+.head {
+  color: white
+}
+
+
+.type {
+  color: red;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: center;
+  text-shadow: 3px 3px 15px orange;
+}
+
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+}
+
+.card {
+  background-color: orange;
+  border-radius: 10px;
+  margin: 20px;
+  height: 250px;
+  width: 250px;
+  text-align: center;
+  align-content: center;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.card-title {
+  color: red;
+  font-size: 20px;
+  font-weight: bold;
+  text-shadow: 1px 1px 5px black;
+}
+
+.card-description {
+  font-size: 14px;
+  text-align: start;
+  padding-left: 50px;
+}
+</style>
